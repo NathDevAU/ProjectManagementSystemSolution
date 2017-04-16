89d31d90a1431cd525a5b13aa31984c72a3fbec9
Dharmesh Patel <dharmesh020@hotmail.com>
2017-04-16 16:31:09 +1000

    final changes of project management system and functionality verification started

--- PMSAngularApp/ClientApp/app/components/AddProjectPerson/AddProjectPerson.component.ts
+++ PMSAngularApp/ClientApp/app/components/AddProjectPerson/AddProjectPerson.component.ts
@@ -30,14 +30,20 @@ export class AddProjectPersonComponent  {
                 // Defaults to 0 if no query param provided.
                 this.ProjectID = parseInt( params['ProjectID']);
                 this.ProjectName = params['ProjectName'];
-                this.httpService.get('api/PersonData/GetPersonListToAddInProject?ProjectID=' + this.ProjectID).subscribe(result => {
-                    // debugger;
-                    this.Persons = result.json() as Person[];
-                });
+                this.GetPersonListToAddToProject();
+                
             });
     }
     ngOnDestroy() {
        
+    }
+
+    GetPersonListToAddToProject()
+    {
+        this.httpService.get('api/PersonData/GetPersonListToAddInProject?ProjectID=' + this.ProjectID).subscribe(result => {
+            // debugger;
+            this.Persons = result.json() as Person[];
+        });
     }
 
     //Person need to be added into selected Project
@@ -47,15 +53,15 @@ export class AddProjectPersonComponent  {
         headersB.append('Content-Type', 'application/json');
         //let options = new RequestOptions({ headers: headersB }); 
         let options = new RequestOptions({ headers: headersB });
-        alert(personIDSelected);
-        alert(projectIDSelected);
+       // alert(personIDSelected);
+      //  alert(projectIDSelected);
        
         var body = JSON.stringify({ PersonID: personIDSelected, ProjectID: projectIDSelected });
         debugger;
               this.httpService.post('api/ProjectPersonData/PostPersonToProject', body, options).subscribe(result => {
             // debugger;
-           
-
+                   //refresh the current list to make sure recently added person should not appear
+                  this.GetPersonListToAddToProject();
         });
     }
 }
